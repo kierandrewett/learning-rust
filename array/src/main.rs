954363ff296bac965d28e74d9c8d6b829899d18d
EnderDev <xenderpugx@gmail.com>
2020-12-25 21:34:59 +0000

    Automatically update

--- array/src/main.rs
+++ array/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
 
-    let months = ["January", "February", "March", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let months = [
+        "January", 
+        "February", 
+        "March", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 }
