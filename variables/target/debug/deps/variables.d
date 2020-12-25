f43fc35cdd43963681ead74f5dbb634797c89e17
EnderDev <xenderpugx@gmail.com>
2020-12-25 20:41:23 +0000

    Automatically update

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/enderdev/Documents/rust/learning/learning-rust/variables/target/debug/deps/variables: src/main.rs
+
+/Users/enderdev/Documents/rust/learning/learning-rust/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
