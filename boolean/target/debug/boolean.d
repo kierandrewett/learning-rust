c0cdcc1e9d7d4a36ac6eeb861b21cdc788932b3b
EnderDev <xenderpugx@gmail.com>
2020-12-25 21:27:44 +0000

    Automatically update

--- boolean/target/debug/boolean.d
+++
@@ -1,1 +0,0 @@
-/Users/enderdev/Documents/rust/learning/learning-rust/boolean/target/debug/boolean: /Users/enderdev/Documents/rust/learning/learning-rust/boolean/src/main.rs
--- boolean/target/debug/deps/boolean.d
+++
@@ -1,5 +0,0 @@
-/Users/enderdev/Documents/rust/learning/learning-rust/boolean/target/debug/deps/boolean: src/main.rs
-
-/Users/enderdev/Documents/rust/learning/learning-rust/boolean/target/debug/deps/boolean.d: src/main.rs
-
-src/main.rs:
