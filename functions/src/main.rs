49f4e565297b25af3bba955214afc0fdab5dc65d
EnderDev <xenderpugx@gmail.com>
2020-12-25 21:52:55 +0000

    Automatically update

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     println!("Hello world");
 
     give_melon("Kitten");
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x+1;
+    }
 }
 
 fn give_melon(cat_name: &str) {
