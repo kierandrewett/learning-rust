d9a00001a07657e835bdc63375887bcb55fa0bb1
EnderDev <kieran@dothq.co>
2020-12-26 15:59:11 +0000

    Automatically update

--- string/src/main.rs
+++ string/src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     println!("Length of '{}' is {}", s1, len);
 }
 
-fn calculate_length(some_string: String) -> usize {
+fn calculate_length(some_string: &String) -> usize {
     some_string.len()
 }
