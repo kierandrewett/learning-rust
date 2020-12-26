e978b3ce7ee2a7d79086faf292bbb58dbea5bbf7
EnderDev <kieran@dothq.co>
2020-12-26 15:54:14 +0000

    Automatically update

--- string/src/main.rs
+++ string/src/main.rs
@@ -1,21 +1,3 @@
 fn main() {
-    let s1 = gives_ownership();
-
-    let s2 = String::from("me lon");
-
-    let s3 = takes_and_gives_back(s2);
-
-    println!("{}", s1);
-    // println!("{}", s2); // cant use s2 because it's been given to s3
-    println!("{}", s3);
-}
-
-fn gives_ownership() -> String {
-    let some_string = String::from("hello me lon");
-
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+    
 }
